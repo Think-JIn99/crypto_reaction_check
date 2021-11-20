09f150e8074db2142ce3ff3011485e3850273b87
Taejun Moon <junall10022@gmail.com>
2021-11-20 15:55:07 +0900

    scrapper.py 기반으로 scrapper_sql.py 생성 스크랩+베이더+db저장(일단 1달단위)

---
+++ reddit_scrapper/src/scrapper_sql.py
@@ -0,0 +1,124 @@
+from pmaw import PushshiftAPI  # 없으면 설치 해야해요 pip로 설치
+import pandas as pd
+import os.path
+import datetime as dt
+import typer  # 없으면 설치 해야해요 pip로 설치
+import sqlite3
+from dateutil.relativedelta import relativedelta
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+new_words = {
+    'citron': -4.0,
+    'hidenburg': -4.0,
+    'moon': 4.0,
+    'highs': 2.0,
+    'mooning': 4.0,
+    'long': 2.0,
+    'short': -2.0,
+    'call': 4.0,
+    'calls': 4.0,
+    'put': -4.0,
+    'puts': -4.0,
+    'break': 2.0,
+    'tendie': 2.0,
+     'tendies': 2.0,
+     'town': 2.0,
+     'overvalued': -3.0,
+     'undervalued': 3.0,
+     'buy': 4.0,
+     'sell': -4.0,
+     'gone': -1.0,
+     'gtfo': -1.7,
+     'paper': -1.7,
+     'bullish': 3.7,
+     'bearish': -3.7,
+     'bagholder': -1.7,
+     'stonk': 1.9,
+     'green': 1.9,
+     'money': 1.2,
+     'print': 2.2,
+     'rocket': 2.2,
+     'bull': 2.9,
+     'bear': -2.9,
+     'pumping': -1.0,
+     'sus': -3.0,
+     'offering': -2.3,
+     'rip': -4.0,
+     'downgrade': -3.0,
+     'upgrade': 3.0,
+     'maintain': 1.0,
+     'pump': 1.9,
+     'hot': 1.5,
+     'drop': -2.5,
+     'rebound': 1.5,
+     'crack': 2.5,}
+
+class API:
+    def __init__(self, subreddit, start_point, end_point):
+        self.api = PushshiftAPI()  # api 객체 생성
+        self.subreddit = subreddit  # 갤러리 이름
+        self.start_point = dt.datetime.strptime(start_point, '%Y-%m-%d')
+        self.end_point = dt.datetime.strptime(end_point, '%Y-%m-%d')
+
+        self.con = sqlite3.connect("test.db")
+
+    def convert_to_df(self, submissions) -> pd.DataFrame:
+        # 추출할 속성들
+        columns = [
+            'id',
+            'author',
+            'title',
+            'selftext',
+            'created_utc',
+            'num_comments',
+            'score',
+        ]
+        df = pd.DataFrame(submissions)
+        return df[::][columns]
+
+    def extract_subreddit(self):
+        _after = self.start_point  # 시작점을 의미 합니다. 이 시점 이후로 긁어옴
+        end_point = self.end_point  # 끝점을 의미 합니다. 여기까지 긁는 것이 목표
+        while _after <= end_point:
+            _before = _after + relativedelta(months=1)  # 한달 간격으로 데이터를 읽으므로 한달 뒤를 기준으로 잡아준다.
+            try:
+                # 최대 한달동안 업로드된 게시글을 최대 10000개 가져온다.
+                submissions = self.api.search_submissions(subreddit=self.subreddit, limit=10000,
+                                                          before=int(_before.timestamp()),
+                                                          after=int(_after.timestamp()))
+            except:
+                continue
+            submissions_df = self.convert_to_df(submissions)  # 데이터 프레임 형태로 변환
+            vader_title_df = self.get_vader_df(submissions_df)
+            submissions_df['title_vader'] = vader_title_df['compound']
+
+            submissions_df.to_sql(_after.strftime("%Y-%m-%d"), self.con)
+
+            # 시작점을 한달 뒤로 미뤄준다.
+            _after += relativedelta(months=1)
+
+            # 콘솔에 진행상황 출력
+            typer.echo(f"{self.subreddit}: {_after} ~ {_before}:  one epoch complete!!\n")
+
+    def get_vader_df(self, df):
+        analyzer = SentimentIntensityAnalyzer()
+        analyzer.lexicon.update(new_words)
+        title_vader = df['title'].dropna().apply(analyzer.polarity_scores)
+        # df로 변환해 반환해준다.
+        vader_title_df = pd.DataFrame(dict(title_vader)).T
+        return vader_title_df  # vader실행 결과를 반환해준다.
+
+
+def main(subreddit: str, start_point: str, end_point: str):
+    # 스크래퍼 객체 생성
+    api = API(subreddit, start_point, end_point)
+    if not os.path.exists(f'./dataset'):
+        os.mkdir(f'./dataset')
+    typer.echo(f"{api.subreddit}: {api.start_point} ~ {api.end_point} start scrapping")
+    api.extract_subreddit()
+    api.con.close()
+
+
+if __name__ == '__main__':
+    typer.run(main)
+#python scrapper_sql.py Bitcoin 2019-10-01 2019-11-01

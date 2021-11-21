96dc403d3561b1f94cfb1da8893d4c35c6ff6156
2zzzi <2ghun1210@gmail.com>
2021-11-22 00:48:19 +0900

    django model migragtions
    장고 모델 수정 : models.py 에 scrapper 추가

--- Crypto_temp/ShowCryptoTemp/admin.py
+++ Crypto_temp/ShowCryptoTemp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import ShowCryptoTemp, scrapper
+
+
+admin.site.register(ShowCryptoTemp)
+admin.site.register(scrapper)
+
+
 # Register your models here.
--- Crypto_temp/ShowCryptoTemp/models.py
+++ Crypto_temp/ShowCryptoTemp/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
-class ShowCrypto(models.Model):
+class ShowCryptoTemp(models.Model):
     CryptoName = models.CharField(max_length=200)
     CryptoTemperture = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return  self.CryptoName
+
+class scrapper(models.Model):
+    post_id = models.CharField(max_length=10)
+    author = models.CharField(max_length=100)
+    title = models.TextField()
+    selftext = models.TextField()
+    created_utc = models.IntegerField()
+    num_comments = models.IntegerField()
+    score = models.IntegerField()
+    title_vader = models.FloatField()
+
+    def __str__(self):
+        return self.id

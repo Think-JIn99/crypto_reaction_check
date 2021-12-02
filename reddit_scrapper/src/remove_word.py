1ab80183bd80635ff1d08e2da31552cfffc1fe52
Jin <thinkjin99@gmail.com>
2021-12-02 16:30:12 +0900

    update filter

---
+++ reddit_scrapper/src/remove_word.py
@@ -0,0 +1,20 @@
+remove_words = [
+    'free',
+    'install',
+    'download',
+    'app',
+    'wallet',
+    'hardware',
+    'link',
+    'card',
+    'payapl',
+    'mine',
+    'platform',
+    'browser',
+    'site']
+url_patt = "(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}"
+
+def get_word_patt():
+    word_patt = [r"\b" + w + r"+(\w{1,2})?\b" for w in remove_words]
+    word_patt = "|".join(word_patt)
+    return [word_patt, url_patt]

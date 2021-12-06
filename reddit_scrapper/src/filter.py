bf6b9c1e0c276b899d75ffbc6c68e786a97ab5bb
Jin <thinkjin99@gmail.com>
2021-12-06 14:03:28 +0900

    update data

--- reddit_scrapper/src/filter.py
+++ reddit_scrapper/src/filter.py
@@ -3,13 +3,8 @@ import pandas as pd
 import re
 import typer
 import os
-<<<<<<< HEAD
 from apply_vader import get_vader_df #명령어 subreddit title,comment 여부 입력으로 실행
 import create_regex
-=======
-from apply_vader import get_vader_df #vader 값 적용해주는 함수
-from remove_word import get_word_patt
->>>>>>> e60aef7ced4106c05202b17d2be4a55aebc353c7
 def pre_process(raw_df, column_name):
     if "selftext" in raw_df.columns: #제목의 경우 결측치가 많아 제거해 사용합니다.
         raw_df.drop('selftext',axis=1,inplace=True)
@@ -17,7 +12,6 @@ def pre_process(raw_df, column_name):
     df = df.drop_duplicates([column_name],keep='first') #중복 처리를 해준다.
     return df
 
-<<<<<<< HEAD
 def find_pattern(df, patterns, column_name):
     valid_index = [True] * df.shape[0]
     res = [False] * df.shape[0]
@@ -38,21 +32,6 @@ def do_remove(df, column_name):
     patterns.append(create_regex.url_patt)#url 탐지 패턴
     valid_index = find_pattern(df, patterns, column_name) #삭제해야할 행 인덱스
     return df.loc[~valid_index] #삭제
-=======
-def remove_pattern(df, patterns, column_name):
- #   valid_index = [True] * df.shape[0] 인댁싱을 위한 불리언 배열
-    res = [True] * df.shape[0]
-    for patt in patterns:
-        patt_find = df[column_name].str.findall(patt, flags = re.IGNORECASE)
-        valid_index = np.where(patt_find.apply(len) == 0, True, False)
-        res = res & valid_index
-    return res #삭제하지 말아야할 배열의 인덱스들
-
-def get_valid_df(df, column_name):
-    patterns =  get_word_patt() #remove_words.py에 저장된 삭제할 패턴들을 가져온다.
-    valid_index = remove_pattern(df, patterns, column_name) #광고글 삭제 진행
-    return df.loc[valid_index] #광고가 아닌 글로 판정된 글들 df형태로 반환
->>>>>>> e60aef7ced4106c05202b17d2be4a55aebc353c7
     
 def main(subreddit:str, is_title:str):
     #터미널에서 실행할 때 사용하는 부분
@@ -63,7 +42,6 @@ def main(subreddit:str, is_title:str):
         path = f'../dataset/raw_data/comment_data/{subreddit}'
         column_name = 'body'
     for f in os.listdir(path):
-<<<<<<< HEAD
         if f.endswith('.csv'): # csv형태로 끝나는 파일만.
             try:
                 df = pre_process(pd.read_csv(f"{path}/{f}"), column_name)
@@ -86,29 +64,6 @@ def main(subreddit:str, is_title:str):
                 typer.echo(f"{f} has problem")
                 typer.echo(e)
                 continue
-=======
-        try:
-            df = pre_process(pd.read_csv(f"{path}/{f}"), column_name)
-            typer.echo(f"{f}: start processing") #터미널 출력 문구
-            total_cnt = df.shape[0] #삭제한 글의 수를 파악하기 위해 사용
-            df = get_valid_df(df, column_name)
-            #문자 형태의 데이터만 남긴다.
-            df[column_name].replace(r"[^a-zA-Z ]","",regex = True, inplace=True) #모델에서 이모지나 특수문자를 처리 못하기 때문에 추가적으로 삭제
-            df[column_name].replace("", float("NaN"),inplace=True) #이모지나 특수기호로만 이뤄진 글을 결측치로 만들어준다.
-            df = df.dropna() #이모지로만 구성됐던 글들을 전부 제거한다.
-            df['vader'] = get_vader_df(df, column_name)['compound'] #vader 실행
-            removed_cnt = total_cnt - df.shape[0]
-            out_path = f'{path}/output'
-            #파일 작성
-            if not os.path.exists(out_path):
-                    os.mkdir(f'{out_path}')
-            df.to_csv(f"{out_path}/{f}",index=False)
-            typer.echo(f"{f}: end processing (total: {total_cnt} removed: {removed_cnt})")
-        except Exception as e:
-            typer.echo(f"{f} has problem")
-            typer.echo(e)
-            continue
->>>>>>> e60aef7ced4106c05202b17d2be4a55aebc353c7
         
 if __name__ == '__main__':
     typer.run(main)

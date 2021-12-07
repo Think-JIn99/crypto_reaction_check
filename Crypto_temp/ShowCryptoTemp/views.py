faedb5b035281c16bc165ac3c4a9619882ee4790
2zzzi <2ghun1210@gmail.com>
2021-12-07 23:48:25 +0900

    Front-end 수정
    색깔 수정, ada 삭제

--- Crypto_temp/ShowCryptoTemp/views.py
+++ Crypto_temp/ShowCryptoTemp/views.py
@@ -409,20 +409,17 @@ def index(request):
     Bitcoin_temp_Query = ShowCryptoTemp.objects.filter(CryptoName='BitCoin')
     Ethereum_temp_Query = ShowCryptoTemp.objects.filter(CryptoName='Ethereum')
     Doge_temp_Query = ShowCryptoTemp.objects.filter(CryptoName='Doge')
-    AdA_temp_Query = ShowCryptoTemp.objects.filter(CryptoName='ADA')
     Ripple_temp_Query = ShowCryptoTemp.objects.filter(CryptoName='Ripple')
 
     Bitcoin_temp = float(Bitcoin_temp_Query.values('CryptoTemperture')[0]['CryptoTemperture'])
     Ethereum_temp = float(Ethereum_temp_Query.values('CryptoTemperture')[0]['CryptoTemperture'])
     Doge_temp = float(Doge_temp_Query.values('CryptoTemperture')[0]['CryptoTemperture'])
-    AdA_temp = float(AdA_temp_Query.values('CryptoTemperture')[0]['CryptoTemperture'])
     Ripple_temp = float(Ripple_temp_Query.values('CryptoTemperture')[0]['CryptoTemperture'])
 
     context = {
         'Bitcoin_temp': Bitcoin_temp,
         'Ethereum_temp': Ethereum_temp,
         'Doge_temp': Doge_temp,
-        'AdA_temp': AdA_temp,
         'Ripple_temp': Ripple_temp,
     }
     # return HttpResponse(Bitcoin_temp)
